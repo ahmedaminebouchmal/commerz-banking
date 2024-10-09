01286a4b0a4849941569b9f240b24fc149f3518a
ahmedaminebouchmal <bouchmalaabesp@gmail.com>
2024-10-09 14:30:32 +0200

    vercel deploy

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://8c55bd3337926bfcdebd914ee9889f38@o4507816715878400.ingest.de.sentry.io/4507831841259600",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
